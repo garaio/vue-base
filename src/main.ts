489896e65ca8201c583c5e7dd34eafb76c6c57be
Fabian Gander <fabian.gander@garaio.com>
2020-08-21 14:03:40 +0200

    #3: adding pwaState plugin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import store from "./store";
 import "./registerServiceWorker";
 
+// PWA State
+import PwaState from "@/plugins/pwaState";
+Vue.use(PwaState);
+
 Vue.config.productionTip = false;
 
 new Vue({
