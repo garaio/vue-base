537f52d449d3a946c01fef2b35d60dca66e06f35
Fabian Gander <fabian.gander@garaio.com>
2020-08-21 15:09:42 +0200

    Adding my PwaRefreshUi component

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,29 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to the GARAIO Emergency Room Vue.js Base App" />
     <Counter />
+    <PwaRefreshUi />
+    <h2>Version: {{ packageName }} {{ packageVersion }}</h2>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import packageJSON from "@/../package.json";
 import HelloWorld from "./components/HelloWorld.vue";
 import Counter from "@/modules/counterExample/views/Counter.vue";
+import PwaRefreshUi from "@/components/PwaRefreshUi.vue";
 
 @Component({
   components: {
     HelloWorld,
     Counter,
+    PwaRefreshUi,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private packageVersion = packageJSON.version;
+  private packageName = packageJSON.name;
+}
 </script>
 
 <style lang="scss">
