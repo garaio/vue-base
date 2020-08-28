c63c9f1e8ba7379acbff0a23b174071918b3ac08
Fabian Gander <fabian.gander@garaio.com>
2020-08-28 20:46:40 +0200

    Adding v-navigation-drawer as an example of content #2

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@ import store from "./store";
   },
 })
 export default class App extends Vue {
+  icons = ["widgets", "home", "heart"];
+
   created(): void {
     store.dispatch
       .init()
@@ -27,11 +29,46 @@ export default class App extends Vue {
 </script>
 
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to the GARAIO Emergency Room Vue.js Base App" />
-    <Counter />
-  </div>
+  <v-app id="app">
+    <v-navigation-drawer app clipped>
+      <v-list>
+        <v-list-item v-for="i in 3" :key="i" :to="{ path: '/page' + i }">
+          <v-list-item-action>
+            <v-icon>mdi-{{ icons[i - 1] }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Page {{ i }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <img alt="Vuetify logo" src="./assets/logo.svg" height="219" />
+      <HelloWorld msg="Welcome to the GARAIO Emergency Room Vue.js Base App" />
+      <Counter />
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,15 +6,16 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
+    dark: false,
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
-        accent: "#82B1FF",
-        error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107",
+        // primary: "#42b983",
+        // secondary: "#42b983",
+        // accent: "#82B1FF",
+        // error: "#FF5252",
+        // info: "#2196F3",
+        // success: "#4CAF50",
+        // warning: "#FFC107",
       },
     },
   },
