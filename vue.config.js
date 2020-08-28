329fb01783ebfa486aa2221850c688acf32a524f
Fabian Gander <fabian.gander@garaio.com>
2020-08-28 12:36:46 +0200

    VueCli: vue add vuetify (default) #2

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   outputDir: "dist",
   productionSourceMap: false,
-
   css: {
     extract: false,
     sourceMap: true,
     loaderOptions: {
       scss: {
-        prependData: `@import "@/scss/_importIntoComponents.scss";`,
+        prependData: '@import "@/scss/_importIntoComponents.scss";',
       },
     },
   },
+  transpileDependencies: ["vuetify"],
 };
