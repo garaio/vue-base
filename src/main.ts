bb8a81cc3cbcd0b6fd8c0683d60ca00836208ee5
Fabian Gander <fabian.gander@garaio.com>
2022-04-07 23:22:42 +0200

    #3: vue add pwa
    # Conflicts:
    #	package-lock.json
    #	package.json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue, { VNode } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import "./registerServiceWorker";
 
 // Messages
 import message from "@/mixins/message";
