586c0def3db108e49f691e1b70e0b7c588622615
Fabian Gander <fabian.gander@garaio.com>
2022-04-07 20:56:54 +0200

    Removing $Resources stuff

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,6 @@
-// inside src/types.d.ts
 import Vue from "vue";
-import { VueResources } from "./api/translations/VueResources";
 declare module "vue/types/vue" {
   interface Vue {
     $t(value: string, variables?: string[], language?: string): string;
-    $Resources: typeof VueResources;
   }
 }
