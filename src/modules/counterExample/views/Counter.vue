40fb68abdf1f7847c0aa15ed347a5b1619c8ddb6
Fabian Gander <fabian.gander@garaio.com>
2022-04-07 16:55:22 +0200

    Downgrade counter component to not use vuetify (which is not installed here)

--- src/modules/counterExample/views/Counter.vue
+++ src/modules/counterExample/views/Counter.vue
@@ -1,18 +1,12 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-row>
-        <v-col>
-          <h1>Counter Example</h1>
-          <p>
-            This is an example of a Vue.js component integrated with Vuex, in a
-            more modular way.
-          </p>
-          <Counter />
-        </v-col>
-      </v-row>
-    </v-slide-y-transition>
-  </v-container>
+  <div>
+    <h1>Counter Example</h1>
+    <p>
+      This is an example of a Vue.js component integrated with Vuex, in a more
+      modular way.
+    </p>
+    <Counter />
+  </div>
 </template>
 
 <script lang="ts">
