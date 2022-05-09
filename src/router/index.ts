f8b1786d6ee0f037a8d24c608c5b3d2dc2c06e02
Fabian Gander <fabian.gander@garaio.com>
2022-05-09 14:12:47 +0200

    #9 Advanced router setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,32 @@
 import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import HomeView from "./views/HomeView.vue";
+import VueRouter from "vue-router";
+import type { RouteConfig } from "vue-router";
+import APP_ROUTE_NAMES from "@/router/routeNames";
+import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "home",
+    name: APP_ROUTE_NAMES.HOME.OVERVIEW,
     component: HomeView,
   },
   {
     path: "/about",
-    name: "about",
+    name: APP_ROUTE_NAMES.HOME.ABOUT,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "./views/AboutView.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
     path: "/counter",
-    name: "counter",
+    name: APP_ROUTE_NAMES.COUNTER.OVERVIEW,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "counter" */ "./modules/counterExample/views/Counter.vue"),
+    component: () => import(/* webpackChunkName: "counter" */ "../modules/counterExample/views/Counter.vue"),
   },
 ];
 
---
+++ src/router/routeNames.ts
@@ -0,0 +1,9 @@
+export default {
+  HOME: {
+    OVERVIEW: "home-overview",
+    ABOUT: "home-about",
+  },
+  COUNTER: {
+    OVERVIEW: "counter-overview",
+  },
+} as const;
